6ef1acfaff49cc25bcd1ee17b9f092047b8d237f
sankethsj <sankethjain81@gmail.com>
2024-02-04 15:18:55 +0900

    added models

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,35 @@
 from django.contrib import admin
 
-from home.models import Document
+from home.models import Comment, Document, DocumentHistory
 
 # Register your models here.
-admin.register(Document)
+admin.site.register(Comment)
+admin.site.register(DocumentHistory)
+
+
+class DocumentAdmin(admin.ModelAdmin):
+
+    readonly_fields = [
+        "version",
+        "created_at",
+        "created_by",
+        "updated_at",
+        "updated_by",
+    ]
+
+    def save_model(self, request, obj, form, change):
+
+        obj.updated_by = request.user
+
+        if obj.id is None:
+            obj.version = 1.0
+            obj.created_by = request.user
+            obj.updated_by = request.user
+
+        if change:
+            obj.version = float(obj.version) + 0.1
+
+        super().save_model(request, obj, form, change)
+
+
+admin.site.register(Document, DocumentAdmin)
